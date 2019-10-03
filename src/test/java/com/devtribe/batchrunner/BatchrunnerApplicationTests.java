a2b67148117d080b6287166833f02b923c145a45
anderson_sa <anderson_sa@outlook.com>
2019-10-03 09:00:21 -0300

    project setup

---
+++ src/test/java/com/devtribe/batchrunner/BatchrunnerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devtribe.batchrunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatchrunnerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
